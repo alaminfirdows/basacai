8eb9e009a0f929924c39f2ff2efc8691eab691ff
Al-Amin Firdows <alaminfirdows@gmail.com>
2022-08-20 17:57:45 +0600

    refactor & add division

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
 module.exports = {
-    port:process.env.PORT,
-    local_client_app: process.env.URL,
-    remote_client_app: process.env.REMOTE_CLIENT_APP,
-    allowedDomains: (
-        process.env.NODE_ENV === 'production' ?
-        [
-            process.env.REMOTE_CLIENT_APP,
-            process.env.REMOTE_SERVER_API
-        ] : [
-            process.env.URL,
-            process.env.LOCAL_SERVER_API
-        ]
-    )
-}
+	port: process.env.PORT,
+	local_client_app: process.env.URL,
+	remote_client_app: process.env.REMOTE_CLIENT_APP,
+	allowedDomains:
+		process.env.NODE_ENV === 'production'
+			? [process.env.REMOTE_CLIENT_APP, process.env.REMOTE_SERVER_API]
+			: [process.env.URL, process.env.LOCAL_SERVER_API],
+};
--- server.js
+++ server.js
@@ -1,56 +1,76 @@
-require("dotenv").config({ path: "./config.env" })
-const express = require("express")
-const mongoose = require("mongoose")
-const cors = require("cors")
-const fileUpload = require("express-fileupload")
-const cookieParser = require("cookie-parser")
-const cloudianry = require("cloudinary")
-const http = require("http")
-const helmet = require("helmet")
-const compression = require("compression")
-const { allowedDomains } = require("./index")
+require('dotenv').config({ path: './config.env' });
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const fileUpload = require('express-fileupload');
+const cookieParser = require('cookie-parser');
+const cloudianry = require('cloudinary');
+const http = require('http');
+const helmet = require('helmet');
+const compression = require('compression');
+const { allowedDomains } = require('./index');
 
+const app = express();
+app.use(
+	helmet.contentSecurityPolicy({
+		useDefaults: true,
+		directives: {
+			'img-src': ["'self'", 'https: data:'],
+		},
+	})
+);
+app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
 
+app.use(compression());
+app.use(express.json());
+app.use(cors({ origin: allowedDomains, credentials: true }));
+app.use(cookieParser());
+app.use(
+	fileUpload({
+		useTempFiles: true,
+	})
+);
 
-const app = express()
-app.use(helmet())
-app.use(compression())
-app.use(express.json())
-app.use(cors({origin: allowedDomains,credentials:true}))
-app.use(cookieParser())
-app.use(fileUpload({
-    useTempFiles: true
-}))
+app.use('/api/ping', async (req, res) => {
+	res.status(200).json({ msg: 'pong' });
+});
 
-
-app.use("/api", require("./routes/homesRoutes"))
-app.use("/user", require("./routes/userRoutes"))
-app.use("/api", require("./routes/uploadRoutes"))
-app.use("/api", require("./routes/newsRoutes"))
+app.use('/api', require('./routes/homesRoutes'));
+app.use('/user', require('./routes/userRoutes'));
+app.use('/api', require('./routes/uploadRoutes'));
+app.use('/api', require('./routes/newsRoutes'));
 
 cloudianry.config({
-    cloud_name: process.env.CLOUD_NAME,
-    api_key: process.env.CLOUD_API_KEY,
-    api_secret: process.env.CLOUD_API_SECRET
-})
+	cloud_name: process.env.CLOUD_NAME,
+	api_key: process.env.CLOUD_API_KEY,
+	api_secret: process.env.CLOUD_API_SECRET,
+});
 
-const PORT = process.env.PORT
-const URI = process.env.MONGODB_URL
+const PORT = process.env.PORT;
 
-server = http.createServer(app)
+// database connection
+mongoose
+	.connect(process.env.MONGODB_URL)
+	.then((db) => console.log('Database is connected'))
+	.catch((err) => console.log(err));
 
-mongoose.connect(URI, err => {
-    if (err) throw err;
-    console.log("conected to mongodb");
-})
+// attach client
+if (process.env.NODE_ENV !== 'production') {
+	app.use(express.static(path.resolve(__dirname, 'client/build')));
+	app.get('*', (req, res) => {
+		res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+	});
+} else {
+	app.use(express.static(path.resolve(__dirname, 'public')));
 
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('client/build'))
-    app.get('*',(req,res)=>{
-        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
-    })
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+	});
 }
 
+// run server
+server = http.createServer(app);
 server.listen(PORT, () => {
-    console.log(`Server is running on port no ${PORT}`);
-})
+	console.log(`Server is running on port no ${PORT}`);
+});
